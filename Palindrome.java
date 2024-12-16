16194d56bb881f7a420885e298f630746e36afa1
Harshitha <sindhuharshitha7@gmail.com>
2024-12-16 09:42:19 +0530

    javadocker

--- Palindrome.java
+++ Palindrome.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
 public class Palindrome{
 	public static void maim(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int num = nextInt(),rev = 0,temp = num;
-		while(n>rev)rev=rev*10+n%10,n/10=0;
-		System.out.println(temp==rev? "Palindrome:" "Not palindrome:");
-		sc.close();
+	System.out.println("Palindrome");	
 	}
 }
