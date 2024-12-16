a26918ae39b41acabef193fa23bbb34319713296
Harshitha <sindhuharshitha7@gmail.com>
2024-12-16 09:50:50 +0530

    javadocker

--- Palindrome.java
+++ Palindrome.java
@@ -1,10 +1,5 @@
 public class PalindromeNumber {
     public static void main(String[] args) {
-        int num = 101; 
-        while (num != Integer.parseInt(new StringBuilder(String.valueOf(num)).reverse().toString())) {
-            num++;
-        }
-        System.out.println("Palindrome: " + num);
+        System.out.println("Palindrome");
     }
 }
-
